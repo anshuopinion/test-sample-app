364b5ecec4083a9c7a8422a8e8ef257caf532eff
Anshu Raj <anshusopinion@gmail.com>
2022-03-17 19:55:06 +0530

    date interval implemented

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -9,9 +9,11 @@ import {
   Tooltip,
   Legend,
 } from "chart.js";
+
 import { Line } from "react-chartjs-2";
 import faker from "faker";
-
+import { Box, Button, Container, Flex, Stack } from "@chakra-ui/react";
+import { eachDayOfInterval, format, subDays } from "date-fns";
 ChartJS.register(
   CategoryScale,
   LinearScale,
@@ -22,41 +24,82 @@ ChartJS.register(
   Legend
 );
 
-export const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: "top" as const,
-    },
-    title: {
-      display: true,
-      text: "Chart.js Line Chart",
-    },
-  },
-};
-
-const labels = ["January", "February", "March", "April", "May", "June", "July"];
-
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: "Dataset 1",
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-      borderColor: "rgb(255, 99, 132)",
-      backgroundColor: "rgba(255, 99, 132, 0.5)",
-    },
-    {
-      label: "Dataset 2",
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-      borderColor: "rgb(53, 162, 235)",
-      backgroundColor: "rgba(53, 162, 235, 0.5)",
-    },
-  ],
-};
+// const labels = ["January", "February", "March", "April", "May", "June", "July"];
 
 const Home = () => {
-  return <Line options={options} data={data} />;
+  const [dayInterval, setDateInterval] = useState(30);
+  const [labels, setLabels] = useState<string[]>([]);
+
+  // get last 30 days
+
+  useEffect(() => {
+    const result = eachDayOfInterval({
+      end: new Date(),
+      start: new Date(subDays(new Date(), dayInterval)),
+    }).map((date) => format(date, "MMM dd"));
+    setLabels(result);
+  }, [setLabels, dayInterval]);
+
+  console.log(
+    labels,
+    labels.map(() => faker.datatype.number({ min: 0, max: 50 }))
+  );
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "AAVE v2",
+        data: labels.map(() => faker.datatype.number({ min: 0, max: 50 })),
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+      {
+        label: "Compound",
+        data: labels.map(() => faker.datatype.number({ min: 0, max: 50 })),
+        borderColor: "rgb(53, 162, 235)",
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+      },
+    ],
+  };
+
+  return (
+    <Container maxW={"container.lg"}>
+      <Stack boxShadow="xl" borderRadius="xl" p="4">
+        <Flex justify="space-between">
+          <Flex></Flex>
+          <Flex gap={4}>
+            {[30, 60, 90].map((days) => (
+              <Box
+                key={days}
+                onClick={() => setDateInterval(days)}
+              >{`${days}D`}</Box>
+            ))}
+          </Flex>
+        </Flex>
+        <Flex>
+          {labels.length > 0 && (
+            <Line
+              options={{
+                responsive: true,
+                plugins: {
+                  legend: {
+                    // onClick: () => {},
+                    position: "bottom" as const,
+                  },
+                  title: {
+                    display: false,
+                    text: "Chart.js Line Chart",
+                  },
+                },
+              }}
+              data={data}
+            />
+          )}
+        </Flex>
+      </Stack>
+    </Container>
+  );
 };
 
 export default Home;
